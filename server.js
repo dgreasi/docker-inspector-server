e2f59d52ef9b53715b8809f21d4117901db9dc6c
Dimitris Greasidis <dgreasidis@gmail.com>
2019-06-06 19:48:10 +0300

    install cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  cors = require('cors');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var routes = require('./routes/dockerRoutes'); //importing route
 routes(app); //register the route
